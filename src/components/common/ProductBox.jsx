f0f8c6d7f19d95a3e8a653a2b1abdb999de39473
vancrow16 <vancrow161989@gmail.com>
2023-07-15 19:46:33 +0800

    uypdated

--- src/components/common/ProductBox.jsx
+++ src/components/common/ProductBox.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import Numeral from "react-numeral";
+import { baseUrl } from "../../../config.json";
 
 function ProductBox({ product }) {
   const { id, name, price, image } = product;
@@ -8,7 +9,7 @@ function ProductBox({ product }) {
       to={`/product-list/${id}`}
       href="#"
       className="mb-8 block border border-gray-300 shadow-2xl hover:opacity-80 md:mb-16">
-      <img className="h-auto w-full" src={image} alt={name} />
+      <img className="h-auto w-full" src={baseUrl + image} alt={name} />
       <div className="p-3">
         <p className="mb-1 font-semibold ">{name}</p>
         <p>
