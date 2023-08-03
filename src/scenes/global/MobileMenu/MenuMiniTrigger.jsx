a24ad7a68e6a36d8de2ed5354a7eb27e69cdf0cf
vancrow16 <vancrow161989@gmail.com>
2023-08-03 22:08:14 +0800

    adjusted menu trigger size

--- src/scenes/global/MobileMenu/MenuMiniTrigger.jsx
+++ src/scenes/global/MobileMenu/MenuMiniTrigger.jsx
@@ -4,7 +4,7 @@ function MenuMiniTrigger({ setOpenMenu }) {
   return (
     <button
       onClick={() => setOpenMenu((prevValue) => !prevValue)}
-      className="inline-block rounded-sm text-white md:hidden">
+      className="inline-block h-9  w-9  rounded-sm text-white md:hidden">
       <Bars3Icon className="inline-block w-6 align-middle" />
     </button>
   );
